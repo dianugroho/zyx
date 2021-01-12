dfc26d3d4188adeda37e6de19780edcf5ee542ab
dianugroho <dianugroho31@gmail.com>
2021-01-13 01:44:58 +0700

    Create roles

---
+++ begin/app/Models/Roles.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuids;
+
+class Roles extends Model
+{
+    use HasFactory, Uuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'role',
+    ];
+}
